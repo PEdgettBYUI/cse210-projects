b626be7ab8e844b08fca5f232a72a883acd15023
ContraZombie4 <102491665+ContraZombie4@users.noreply.github.com>
2025-04-26 23:55:51 -0600

    Prep 1 & 2 Complete.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("This is the Pass-Checker.");
+        Console.WriteLine("Here we'll take your number grade and tell you what letter grade you recieved, and if you passed. \n");
+
+        // Determining the letter grade
+        Console.WriteLine("What was your grade? %");
+        string userInput = Console.ReadLine();
+        int gradePercent = int.Parse(userInput);
+        string letterGrade = "";
+
+        if(gradePercent >= 90)
+        {
+            // Console.WriteLine("You got a A!");
+            letterGrade = "A";
+
+            // if(gradePercent)
+            // {
+            //     Console.WriteLine('You Got an "B+" !');
+            // }
+        }
+        else if (gradePercent >= 80)
+        {
+            // Console.WriteLine("You got a B!");
+            letterGrade = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            // Console.WriteLine("You got a C.");
+            letterGrade = "C";
+        }   
+        else if (gradePercent >= 70)
+        {
+            // Console.WriteLine("You got a D...");
+            letterGrade = "D";
+        }
+        else
+        {
+            // Console.WriteLine("You got an F.");
+            letterGrade = "F";
+        }
+
+        // Determine if the user passed
+
+        Console.WriteLine($"Your Grade was an {letterGrade}.");
+
+        if(gradePercent >= 70)
+        {
+            Console.WriteLine("You Passed! Good Job!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't Pass. Study more next time.\n");
+        }
+
+
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,19 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
-        // This is a test.
-        // This is a second change.
-        Console.Write("What is your first name? "); //Write - Cursor Same Line; WriteLine - Cursor Next Line
-        string firstName = Console.ReadLine();
-        Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
 
         int x = 10;
         // if(x==9);   //Do not put ; at end of if statements
         {
-            Console.WriteLine
+            Console.WriteLine("END");
         }
-
-    }
 }
+    }
