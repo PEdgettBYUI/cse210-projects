886011036b74234611c1115f0c7b5cf9eaeb5cb8
ContraZombie4 <gameking599@gmail.com>
2025-06-20 10:27:37 -0600

    Functioning Menu;

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Menu mainmenu = new Menu();
+
+        int choice = 0;
+        while (choice != 4)
+        {
+            mainmenu.displayMenu();
+
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+                    
+                default:
+                    Console.WriteLine("Please input a number from 1-4.");
+                    break;
+            }
+        }
+
     }
 }
